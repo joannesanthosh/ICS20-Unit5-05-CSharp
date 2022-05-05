b3fef63148e839e8d3755e6428b033ff9acc0ebf
Joanne Santhosh <99355156+joannesanthosh@users.noreply.github.com>
2022-05-05 17:28:19 +0000

    change main.cs

--- main.cs
+++ main.cs
@@ -1,7 +1,30 @@
+// Created by: Joanne Santhosh
+// Created on: May 2022
+//
+// This program identifies the type of triangle
+
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+     public static void Main(string[] args)
+    {
+        // This program identifies the type of triangle
+        int sideAOfTriangle;
+        int sideBOfTriagle;
+        int sideCOfTriangle;
+        Console.WriteLine("This program identifies the type of triangle");
+        Console.WriteLine("");
+
+        Console.WriteLine("Enter side a of the triangle.");
+        sideAOfTriangle = (Convert.ToInt32(Console.ReadLine()));
+        Console.Write("Enter side b of the triangle. ");
+        sideBOfTriagle = (Convert.ToInt32(Console.ReadLine()));
+        Console.WriteLine("Enter side c of the triangle.");
+        sideCOfTriangle = (Convert.ToInt32(Console.ReadLine()));
+        Console.WriteLine("");
+        Console.WriteLine("Area is: " + (lengthOfTriangle * widthOfTriangle * heightOfTriangle / 3) + " cm³ ");
+
+        Console.WriteLine("\nDone.");
+    }
 }
