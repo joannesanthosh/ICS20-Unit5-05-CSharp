296d641b3e5d9742aa1546d820400d3a4f14e62e
Joanne Santhosh <99355156+joannesanthosh@users.noreply.github.com>
2022-05-05 23:14:40 +0000

    fix main.cs

--- main.cs
+++ main.cs
@@ -7,7 +7,7 @@ using System;
 
 class Program
 {
-     public static void Main(string[] args)
+      public static void Main(string[] args)
     {
         // This program identifies the type of triangle
         int sideAOfTriangle;
